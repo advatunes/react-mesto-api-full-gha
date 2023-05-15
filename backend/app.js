b5370bb348bee23b5fb797982a46ece679528a69
advatunes <dj-9-1-1@yandex.ru>
2023-05-15 11:14:32 +0300

    update cors

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,7 @@ app.use(cors({
   origin: 'https://advatunes.mesto.nomoredomains.monster',
   credentials: true
 }));
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
