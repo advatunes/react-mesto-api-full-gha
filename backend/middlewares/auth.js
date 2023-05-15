87531eed908d48673a1363eb02a1be5ed6555962
advatunes <dj-9-1-1@yandex.ru>
2023-05-15 21:58:12 +0300

    update jwt auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,24 +3,20 @@ const config = require("../config");
 const { STATUS_INVALID_CREDENTIALS } = require("../utils/errors");
 const {  JWT_SECRET } = process.env;
 
-module.exports = (req, res, next) => {
-  const { cookies } = req;
 
-  if (!cookies || !cookies.jwt) {
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
     return next(new STATUS_INVALID_CREDENTIALS("Необходима авторизация"));
   }
 
-  const token = cookies.jwt;
+  const token = authorization.replace("Bearer ", "");
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, JWT_SECRET );
   } catch (err) {
     return next(new STATUS_INVALID_CREDENTIALS("Необходима авторизация"));
-  }
-
-  req.user = payload;
-
-  next();
+  } next();
 };
-
