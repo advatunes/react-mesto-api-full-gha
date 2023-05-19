40f2651787b2fbc1a3a1269308f2093c3c2e524e
advatunes <dj-9-1-1@yandex.ru>
2023-05-20 00:38:05 +0300

    update cors

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,13 @@ mongoose.connect(config.mongoUri, {
   useUnifiedTopology: true,
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://advatunes.mesto.nomoredomains.monster');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(cors({
   origin: ['https://advatunes.mesto.nomoredomains.monster', 'http://localhost:3000'],
   credentials: true
