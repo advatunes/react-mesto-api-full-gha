7b83f644b459b51c51b00d9b8567af63672040ac
advatunes <dj-9-1-1@yandex.ru>
2023-05-21 21:27:20 +0300

    update cors

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3000,
-  mongoUri: "mongodb://localhost:27017/mestodb",
+  mongoUri: "mongodb://127.0.0.1:27017/mestodb",
 };
 
 module.exports = config;
